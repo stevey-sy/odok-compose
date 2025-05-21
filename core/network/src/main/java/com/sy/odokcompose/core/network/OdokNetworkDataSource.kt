c812c92f9374b93562f144581ad89fe16abc7885
sinsy8989 <sinsy89@monkey-soft.com>
2025-05-22 01:17:43 +0900

    core/network, data -> 책 상세 정보 받아오기 api 추가

--- core/network/src/main/java/com/sy/odokcompose/core/network/OdokNetworkDataSource.kt
+++ core/network/src/main/java/com/sy/odokcompose/core/network/OdokNetworkDataSource.kt
@@ -1,8 +1,10 @@
 package com.sy.odokcompose.core.network
 
+import com.sy.odokcompose.core.network.model.AladinBookDetailResponse
 import com.sy.odokcompose.core.network.model.AladinBookItem
 import com.sy.odokcompose.core.network.model.AladinBookSearchResponse
 
 interface OdokNetworkDataSource {
     suspend fun getSearchedBooks(query: String, start: Int, maxResults: Int): AladinBookSearchResponse
+    suspend fun getBookDetail(itemId: String): AladinBookDetailResponse
 }
--- core/network/src/main/java/com/sy/odokcompose/core/network/service/AladinApiService.kt
+++ core/network/src/main/java/com/sy/odokcompose/core/network/service/AladinApiService.kt
@@ -1,7 +1,9 @@
 package com.sy.odokcompose.core.network.service
 
 import com.sy.odokcompose.core.network.BuildConfig
+import com.sy.odokcompose.core.network.model.AladinBookDetailResponse
 import com.sy.odokcompose.core.network.model.AladinBookSearchResponse
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -22,6 +24,16 @@ interface AladinApiService {
         @Query("Output") output: String = "JS",
         @Query("Version") version: String = "20131101"
     ): AladinBookSearchResponse
+
+    @GET("ItemLookUp.aspx")
+    suspend fun getBookDetail(
+        @Query("ttbkey") ttbKey: String = BuildConfig.ALADIN_TTB_KEY,
+        @Query("itemIdType") itemIdType: String = "ISBN",
+        @Query("ItemId") itemId: String, // 기본값 설정
+        @Query("Cover") cover: String = "Big",
+        @Query("Output") output: String = "JS",
+        @Query("Version") version: String = "20131101"
+    ): Response<AladinBookDetailResponse>
     
     companion object {
         const val BASE_URL = "https://www.aladin.co.kr/ttb/api/"
