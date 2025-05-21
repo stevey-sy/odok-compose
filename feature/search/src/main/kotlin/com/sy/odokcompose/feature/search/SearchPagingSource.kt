31e1de5ed2235e98b3a0ccffbbb5cf972e73b365
sinsy8989 <sinsy89@monkey-soft.com>
2025-05-21 21:16:31 +0900

    feature/search -> pagingSource 추가

---
+++ feature/search/src/main/kotlin/com/sy/odokcompose/feature/search/SearchPagingSource.kt
@@ -0,0 +1,36 @@
+package com.sy.odokcompose.feature.search
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.sy.odokcompose.core.data.repository.BookRepository
+import com.sy.odokcompose.model.SearchBookUiModel
+
+class SearchPagingSource(
+    private val bookRepository: BookRepository,
+    private val query: String
+) : PagingSource<Int, SearchBookUiModel>() {
+
+    override fun getRefreshKey(state: PagingState<Int, SearchBookUiModel>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, SearchBookUiModel> {
+        return try {
+            val page = params.key ?: 1
+            val pageSize = params.loadSize
+            
+            val response = bookRepository.searchBooks(query, page, pageSize)
+            
+            LoadResult.Page(
+                data = response,
+                prevKey = if (page > 1) page - 1 else null,
+                nextKey = if (response.size == pageSize) page + 1 else null
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+} 
