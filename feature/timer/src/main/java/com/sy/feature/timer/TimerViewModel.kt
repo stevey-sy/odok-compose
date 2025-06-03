cc8c43b40a753b67f817af1f00048e1ee4c5edbc
sinsy8989 <sinsy89@monkey-soft.com>
2025-06-03 18:10:10 +0900

    timer -> timerViewModel 추가

--- feature/timer/src/main/java/com/sy/feature/timer/TimerViewModel.kt
+++ feature/timer/src/main/java/com/sy/feature/timer/TimerViewModel.kt
@@ -1,4 +1,23 @@
 package com.sy.feature.timer
 
-class TimerViewModel {
+import androidx.lifecycle.ViewModel
+import com.sy.odokcompose.core.domain.GetBookDetailUseCase
+import com.sy.odokcompose.core.domain.UpdateBookUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+sealed interface TimerUiState {
+    data object BeforeReading : TimerUiState
+    data object Reading : TimerUiState
+    data object Paused : TimerUiState
+    data object Completed : TimerUiState
+}
+
+class TimerViewModel @Inject constructor(
+    private val getBookDetailUseCase: GetBookDetailUseCase,
+    private val updateBookUseCase: UpdateBookUseCase,
+) : ViewModel() {
+    private val _uiState = MutableStateFlow<TimerUiState>(TimerUiState.BeforeReading)
+    val uiState: StateFlow<TimerUiState> = _uiState
 }
