105332dc757d01e44924585c298e82a8309d1db9
sinsy8989 <sinsy89@monkey-soft.com>
2025-06-20 21:39:31 +0900

    firebase 연동 작업 중

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt.plugin)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -81,4 +82,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.firebase.bom)
+    implementation(libs.firebase.analytics)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt.plugin) apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
